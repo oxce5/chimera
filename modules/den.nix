da5400b73d6347febdfa7f8921ab772d7950fc09
avg.gamer <avg.gamer@proton.me>
2026-07-12 15:42:55 +0800

    fix hm

--- modules/den.nix
+++ modules/den.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   _module.args.__findFile = den.lib.__findFile;
-  den.schema.user.includes = [ den._.mutual-provider ];
+  den.schema.user.includes = [den._.mutual-provider];
   flake.den = den;
   imports = [
     inputs.den.flakeModule

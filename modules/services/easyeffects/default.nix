44f5d55a4b691dd330e47ef2bc6aa3948c4504e2
avg.gamer <avg.gamer@proton.me>
2026-07-12 15:44:11 +0800

    add presets

--- modules/services/easyeffects/default.nix
+++ modules/services/easyeffects/default.nix
@@ -1,28 +1,23 @@
-{ lib, ... }:
-{
+{lib, ...}: {
   chimera.easyeffects.homeManager = {
     # systemd.user.services.easyeffects.Service.Restart = lib.mkForce "never";
     services.easyeffects = {
       enable = true;
-      # extraPresets =
-      #   let
-      #     presets = [
-      #       "AKG K7XX"
-      #       "Hifiman Edition XS"
-      #       "Moondrop Starfield"
-      #       "Nothing Ear (a)"
-      #       "7RYMS"
-      #     ];
-      #     mkPresets =
-      #       names:
-      #       builtins.listToAttrs (
-      #         map (name: {
-      #           inherit name;
-      #           value = builtins.fromJSON (builtins.readFile (./presets + "/${name}.json"));
-      #         }) names
-      #       );
-      #   in
-      #   mkPresets presets;
+      extraPresets = let
+        presets = [
+          "Default"
+          "KZ Castor Pro"
+        ];
+        mkPresets = names:
+          builtins.listToAttrs (
+            map (name: {
+              inherit name;
+              value = builtins.fromJSON (builtins.readFile (./presets + "/${name}.json"));
+            })
+            names
+          );
+      in
+        mkPresets presets;
     };
   };
 }

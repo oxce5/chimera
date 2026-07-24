c91d37875babe7b1b6ebd3c6264c04fe51cfb139
avg.gamer <avg.gamer@proton.me>
2026-07-24 12:37:42 +0800

    fix lvm

--- modules/hosts/machina-mori/filesystems.nix
+++ modules/hosts/machina-mori/filesystems.nix
@@ -1,7 +1,23 @@
-{
+{pkgs, ...}: {
   den.aspects.machina-mori.nixos = {
+    boot.initrd.services.lvm.enable = true;
+    boot.initrd.availableKernelModules = ["virtio_blk" "virtio_pci" "virtio"];
+    boot.initrd.kernelModules = ["dm_mod"];
+
+    # boot.initrd.systemd.services.lvm-activate = {
+    #   description = "Activate LVM volume group pool";
+    #   wantedBy = ["initrd.target"];
+    #   after = ["systemd-udevd.service"];
+    #   before = ["sysroot.mount"];
+    #   unitConfig.DefaultDependencies = false;
+    #   serviceConfig.Type = "oneshot";
+    #   script = ''
+    #     ${pkgs.lvm2}/bin/lvm vgchange -ay pool
+    #   '';
+    # };
+
     fileSystems."/" = {
-      device = "/dev/disk/by-label/ROOT";
+      device = "/dev/pool/NIXROOT";
       fsType = "ext4";
     };
 

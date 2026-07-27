23d0a5e80cba47c0273b2fc06e66c5a8c69a298c
avg.gamer <avg.gamer@proton.me>
2026-07-27 16:14:50 +0800

    formatting

--- modules/cachix.nix
+++ modules/cachix.nix
@@ -5,14 +5,14 @@
       key = "${name}.cachix.org-1:${key}";
     };
     cachix = {
-      chaotic-nyx    = mkCachix "chaotic-nyx" "HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=";
-      nix-community  = mkCachix "nix-community" "mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
-      nix-gaming     = mkCachix "nix-gaming" "nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=";
-      prismlauncher  = mkCachix "prismlauncher" "9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c=";
-      devenv         = mkCachix "devenv" "w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-      niri-nix       = mkCachix "niri-nix" "SvFtqpDcf7Sm1SMJdby1/+Y+6f3Yt3/3PMcSTKPJNJ0=";
-      kopuz          = mkCachix "kopuz" "J2X3AnAYhKTJW5S3aCLoA1ckonQXVNZMQvhZA0YAufw=";
-      noctalia       = mkCachix "noctalia" "pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=";
+      chaotic-nyx = mkCachix "chaotic-nyx" "HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=";
+      nix-community = mkCachix "nix-community" "mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+      nix-gaming = mkCachix "nix-gaming" "nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=";
+      prismlauncher = mkCachix "prismlauncher" "9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c=";
+      devenv = mkCachix "devenv" "w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+      niri-nix = mkCachix "niri-nix" "SvFtqpDcf7Sm1SMJdby1/+Y+6f3Yt3/3PMcSTKPJNJ0=";
+      kopuz = mkCachix "kopuz" "J2X3AnAYhKTJW5S3aCLoA1ckonQXVNZMQvhZA0YAufw=";
+      noctalia = mkCachix "noctalia" "pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=";
     };
   in {
     nix.settings = {

2cbc749af976426dc9f516ebb00a8910979c2171
avg.gamer <avg.gamer@proton.me>
2026-09-20 15:19:21 +0800

    formatting

--- modules/pwn/c2.nix
+++ modules/pwn/c2.nix
@@ -3,13 +3,15 @@
   chimera,
   ...
 }: {
-  chimera.pwn.provides.c2.nixos = {
+  chimera.pwn.provides.c2 = {
+    nixos = {
       imports = [inputs.chimera-pkgs.nixosModules.sliver];
       services.sliver.enable = true;
-  };
-  homeManager = {pkgs, ...}: {
+    };
+    homeManager = {pkgs, ...}: {
       home.packages = with pkgs; [
-          penelope
+        penelope
       ];
-  }
+    };
+  };
 }
